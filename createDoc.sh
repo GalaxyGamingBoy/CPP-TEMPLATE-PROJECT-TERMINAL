8f7ed979b914564d97cee58f234d731ba7895ba7
GalaxyGamingBoy <xrteach@gmail.com>
2022-09-17 17:51:46 +0300

    Added createDoc.sh

---
+++ createDoc.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+mkdir docs
+GITREPO=https://github.com/GalaxyGamingBoy/CPP-TEMPLATE-PROJECT-TERMINAL
+cd docs
+git clone $GITREPO
+cd CPP-TEMPLATE-PROJECT-TERMINAL
+git checkout -b docs
+mkdir web
+mv docsRedirect.html index.html
+git add *
+git commit -m "chore: Update Doxygen Docs"
+git push --force $GITREPO docs
+cd ../..
+rm -rf docs
+(END)
